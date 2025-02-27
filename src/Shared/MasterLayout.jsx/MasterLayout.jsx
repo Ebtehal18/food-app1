2f0147dae1510b8314ec24374ad044bbae62a645
Ebtehal18 <ebtehallelsayed18@gmail.com>
2025-02-27 19:10:16 +0200

    fix sidebar import

--- src/Shared/MasterLayout.jsx/MasterLayout.jsx
+++ src/Shared/MasterLayout.jsx/MasterLayout.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
 import SideBar from "../Sidebar/SideBar";
+
 
 
 export default function MasterLayout({adminData}) {
