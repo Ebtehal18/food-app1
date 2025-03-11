53ee829bbb6ac2a35fa1b105edf785983786e337
Ebtehal18 <ebtehallelsayed18@gmail.com>
2025-03-11 14:43:10 +0200

    add verify ui

--- src/Authentication/Verify-account/VerifyAccount.jsx
+++ src/Authentication/Verify-account/VerifyAccount.jsx
@@ -1,5 +1,52 @@
 import React from "react";
 
 export default function VerifyAccount() {
-  return <div>VerifyAccount</div>;
+  return <>
+   <div className="title my-3">
+                <h3 className="h5"> Verify Account  </h3>
+                <p className="text-muted">Please Enter Your Otp  or Check Your Inbox</p>
+              </div>
+
+
+            <form >
+            {/* email input */}
+            <div className="input-group mb-3">
+             <div className="input-group-prepend">
+              <span className="input-group-text h-100" id="basic-addon1">
+              <i class="fa-solid fa-mobile-screen"></i>
+            </span>
+           </div>
+           <input 
+      
+            type="email" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1"/>
+            </div>
+            {/* {errors.email && <div className=" mb-3 alert-danger alert">{errors.email.message}</div>} */}
+
+            {/* password input */}
+            <div className="input-group mb-3 ">
+             <div className="input-group-prepend">
+              <span className="input-group-text h-100" id="basic-addon1">
+              <i className="fa fa-key"></i>
+            </span>
+           </div>
+           <input 
+        
+          type={'text'} className="form-control pass-input" placeholder="OTP" aria-label="OTP" aria-describedby="basic-addon1"/>
+          <span className="btn btn-outline-secondary  border-start-0 border-secoundry-subtle" type='button'
+          // onClick={()=>setShowPassword(!showPassword)}
+          >
+           {/* <i className={`fa-solid ${showPassword?"fa-eye":"fa-eye-slash"}`}></i> */}
+          </span>
+            </div>
+            {/* {errors.password && <div className="mb-3 alert-danger alert">{errors.password.message}</div>} */}
+
+
+          
+
+
+              <button className=" text-white w-100 btn-form">
+               Send</button>
+            </form>
+    
+  </>
 }
--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -20,7 +20,7 @@ export default function Dashboard() {
 <div className="d-flex details container-fluid  px-md-4 px-2 ">
     <div className='w-100  d-flex flex-md-row flex-column justify-content-between py-2 py-md-5  px-3 rounded-3 align-items-center dashboard-header'>
     <div className="caption mb-2 mb-md-0">
-      <h4 className="mb-0">Fill the<span>Recipes</span>! </h4>
+      <h4 className="mb-0">Fill the <span>Recipes</span>! </h4>
       <p className="mb-0">you can now fill the meals easily using the table and form , click here and sill it with the table !</p>
     </div>   
     <Link to={'/dashboard/recipes'} className={" btn-add px-3 py-2 text-decoration-none  px-md-5 py-md-3  text-white fw-bold"}>
