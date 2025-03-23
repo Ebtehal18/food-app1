23696a7b25838021827bf33c1f2f650eb0200757
Ebtehal18 <ebtehallelsayed18@gmail.com>
2025-03-23 12:57:49 +0200

    modify image-no-data responsive

--- src/Shared/Nodata/Nodata.jsx
+++ src/Shared/Nodata/Nodata.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NoImgData from '../../assets/images/no-data1.png'
 export default function Nodata() {
   return <div className="text-center">
-    <img src={NoImgData} alt="no data img" />
+    <img src={NoImgData} alt="no data img"  className="no-data-img"/>
     <h4 className="my-3">No Data !</h4>
   </div>
 }
